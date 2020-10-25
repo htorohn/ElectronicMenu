580df92547af9d271cf0ea904c60075cc826202a
htoro <htorohn@gmail.com>
2020-10-25 11:53:41 -0600

    modified Head

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Head from 'next/head'
 import Grid from '@material-ui/core/Grid'
 import Typography from '@material-ui/core/Typography'
 import { makeStyles, useTheme } from '@material-ui/styles'
@@ -26,25 +27,54 @@ const ElectronicMenu = () => {
 	const theme = useTheme()
 
 	return (
-		<Grid container direction='column'>
-			<Grid
-				item
-				container
-				direction='column'
-				alignItems='center'
-				className={classes.imagesContainer}
-			>
-				{images.map((image, index) => (
-					<Grid item key={index} className={classes.imageContainer}>
-						<img
-							alt={`Kfes de Barro Menu Page ${index + 1}`}
-							src={`/assets/${index + 1}.jpg`}
-							className={classes.menuImage}
-						/>
-					</Grid>
-				))}
+		<>
+			<Head>
+				<title key='title'>Kfés de Barro</title>
+				<meta
+					name='description'
+					key='description'
+					content='El mejor ambiente familiar, las mejores crepas y pizzas de Santa Lucía'
+				/>
+				<meta
+					property='og:title'
+					key='og:title'
+					content='Kfés de Barro | Una visita obligada'
+				/>
+				<meta
+					property='og:url'
+					key='og:url'
+					content='https://kfesdebarro.vercel.app/'
+				/>
+				<link
+					rel='canonical'
+					key='canonical'
+					href='https://kfesdebarro.vercel.app/'
+				/>
+			</Head>
+			<Grid container direction='column'>
+				<Grid
+					item
+					container
+					direction='column'
+					alignItems='center'
+					className={classes.imagesContainer}
+				>
+					{images.map((image, index) => (
+						<Grid
+							item
+							key={index}
+							className={classes.imageContainer}
+						>
+							<img
+								alt={`Kfes de Barro Menu Page ${index + 1}`}
+								src={`/assets/${index + 1}.jpg`}
+								className={classes.menuImage}
+							/>
+						</Grid>
+					))}
+				</Grid>
 			</Grid>
-		</Grid>
+		</>
 	)
 }
 
